18dfbdc510c9e1594a01cf2672a7f35adfae61e4
Michele Damiano Torelli <me@mdtorelli.it>
2014-02-19 16:05:32 +0100

    Add initial implementation of GoBari.it website controller (Refs: #6)

---
+++ src/main/scala/it/dtk/controller/GoBariWebSiteController.scala
@@ -0,0 +1,22 @@
+package it.dtk.controller
+
+import it.dtk.WebSiteController
+import akka.actor.Props
+import it.dtk.WebSiteController._
+
+/**
+ * @author Michele Damiano Torelli <daniele@datatoknowledge.it>
+ */
+class GoBariWebSiteController extends WebSiteController {
+
+  override val maxIncrement: Int = 162
+
+  // http://go-bari.it/notizie/cronaca/
+  override val baseUrl: String = "http://go-bari.it/"
+
+  override def dataRecordExtractorProps: Props = ???
+
+  override def logicalListUrlGenerator(start: Int, stop: Int) : Seq[Job] = {
+    start to stop map(v => Job(baseUrl + "index.php?id=0|2&idS=19&pageID=" + v, v))
+  }
+}
