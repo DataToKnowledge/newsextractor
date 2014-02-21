06f5f179e0ee729f0cfd8b0a7b3acbe5b6b75e28
Fabiana Lanotte <fabiana.lan@hotmail.it>
2014-02-21 12:42:45 +0100

    Add missing test resource file (TestArticle.html)

--- src/test/scala/it/dtk/MainContentExtractorSpec.scala
+++ src/test/scala/it/dtk/MainContentExtractorSpec.scala
@@ -24,14 +24,15 @@ with WordSpecLike with Matchers with BeforeAndAfterAll {
       system.actorOf(Props(new StepParent(Props(new MainContentExtractor(rightCanonUrl, lines)), testActor)), "rightContent")
 
       val res = expectMsgClass(classOf[Result])
-      res.record.title should contain (rightTitle)
+      res.record.title.trim should be equals (rightTitle)
       res.record.extractionDate should not be null
-
     }
 
   }
 
   val rightTitle = "Giovani, anziani, asili nido e soldi per il Sud ecco il progetto del governo per l'equità"
   val rightCanonUrl = "http://www.repubblica.it/economia/2012/05/12/news/giovani_anziani_asili_nido_e_soldi_per_il_sud_ecco_il_progetto_del_governo_per_l_equit-34962952/"
-  val lines = scala.io.Source.fromFile("test.txt", "utf-8").getLines().mkString
+  val lines = scala.io.Source.fromFile("src/test/resources/TestArticle.html", "utf-8").getLines().mkString
+
+
 }
