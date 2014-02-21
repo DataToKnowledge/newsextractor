08708f3d846d40bae0c472bde9e53212fef9cfc0
Andrea Scarpino <andrea@datatoknowledge.it>
2014-02-21 21:27:26 +0100

    Fix build

--- src/main/scala/it/dtk/db/DataModel.scala
+++ src/main/scala/it/dtk/db/DataModel.scala
@@ -15,7 +15,7 @@ object DataModel {
  * @author fabiofumarola
  * @param id it is the progressive counter of the news extracted by the actor. the counter is reset to 0 every day
  */
-case class News(@Key("_id") id: ObjectId = new ObjectId, prog: Long, urlWebSite: String, urlNews: String, title: String, summary: String, newsDate: Date,
+case class News(id: Long, urlWebSite: String, urlNews: String, title: String, summary: String, newsDate: Date,
                 text: Option[String] = None, tags: Option[Set[String]] = None, metaDescription: Option[String] = None,
                 metaKeyword: Option[String] = None, canonicalUrl: Option[String] = None,
                 topImage: Option[String] = None) {
