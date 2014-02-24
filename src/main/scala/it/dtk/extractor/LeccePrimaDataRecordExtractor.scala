15b47df3c8f0cbccc5f5266b99c19a6875a8251b
fabiofumarola <fabiofumarola@gmail.com>
2014-02-24 19:47:47 +0100

    add test for bari today e lecce prima

--- src/main/scala/it/dtk/extractor/LeccePrimaDataRecordExtractor.scala
+++ src/main/scala/it/dtk/extractor/LeccePrimaDataRecordExtractor.scala
@@ -6,6 +6,7 @@ import java.util.Date
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Element
 import scala.collection.JavaConversions._
+import it.dtk.DataRecordExtractor._
 
 /**
  * @author Andrea Scarpino <andrea@datatoknowledge.it>
@@ -17,6 +18,8 @@ class LeccePrimaDataRecordExtractor(url: String, html: String, date: Date) exten
   //get the data records
   val records = dataRecordXPath("article[class= post p-small clearfix]") map (
     r => DataRecord(title(r), summary(r), newsUrl(r)))
+    
+  context.parent ! DataRecords(url,date,records)
     
   def title(node: Element) = node.select("header > figure > a > img[alt]").attr("alt")
   
