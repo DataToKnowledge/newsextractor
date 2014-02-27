7e7c34dfb62b39364b85bc3f7a60d26fcee28c61
fabiofumarola <fabiofumarola@gmail.com>
2014-02-27 15:16:32 +0100

    fixes #6

---
+++ src/main/scala/it/dtk/extractor/GoBariDataRecordExtractor.scala
@@ -0,0 +1,31 @@
+package it.dtk.extractor
+
+import it.dtk.DataRecordExtractor
+import it.dtk.DataRecordExtractor.{ DataRecord, DataRecords }
+import java.util.Date
+import org.jsoup.Jsoup
+import scala.collection.JavaConversions._
+import org.jsoup.nodes.Element
+import it.dtk.DataRecordExtractor._
+
+/**
+ * @author Andrea Scarpino <andrea@datatoknowledge.it>
+ */
+class GoBariDataRecordExtractor(url: String, html: String, date: Date) extends DataRecordExtractor {
+
+  implicit val doc = Jsoup.parse(html, url)
+
+  //get the data records
+  val records = dataRecordXPath("div.centro_sx_home_news") map (
+    r => DataRecord(title(r), summary(r), newsUrl(r))) filter(_.title.length() > 0)
+  
+    
+  context.parent ! DataRecords(url,date,records)
+    
+  def title(node: Element) = node.select("div.titolo > a").text
+  
+  def summary(node: Element) = node.select("span.civetta > p").text
+  
+  def newsUrl(node: Element) = node.select("div.titolo > a").attr("href")
+
+}
---
+++ src/test/scala/it/dtk/extractor/GoBariDataRecordExtractorSpec.scala
@@ -0,0 +1,48 @@
+package it.dtk.extractor
+
+import it.dtk.util.MySpec
+import java.util.Date
+import org.joda.time.DateTime
+import akka.actor.Props
+import it.dtk.util.StepParent
+import it.dtk.DataRecordExtractor
+import scala.concurrent.duration._
+import scala.io.Source
+
+object GoBariDataRecordExtractorSpec {
+
+  val url = "http://go-bari.it/notizie/cronaca/"
+
+  val date = DateTime.now().toDate
+
+}
+
+class GoBariDataRecordExtractorSpec extends MySpec("GoBariDataRecordExtractorSpec") {
+
+  import GoBariDataRecordExtractorSpec._
+  import DataRecordExtractor._
+
+  val html = Source.fromFile("./src/test/resources/GoBariCronacaList.html", "iso-8859-1").getLines().mkString
+  
+  "The GoBari record extractor" should {
+    "extract 20 data records" in {
+      val dataRecordProps = Props(classOf[GoBariDataRecordExtractor], url, html, date)
+      val dataRecordActor = system.actorOf(Props(classOf[StepParent], dataRecordProps, testActor))
+      val results = expectMsgClass(15.seconds,classOf[DataRecords])
+
+      assert(results.dataRecords.size == 20)
+      results.dataRecords.foreach(dr =>
+      	assert(dr.title.length() > 0)
+      )
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
