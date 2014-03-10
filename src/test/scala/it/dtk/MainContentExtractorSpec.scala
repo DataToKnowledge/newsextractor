c6002df76e9c6c380267549c11166d969856eb5d
Michele Damiano Torelli <me@mdtorelli.it>
2014-03-10 19:31:40 +0100

    Optimize imports

--- src/test/scala/it/dtk/MainContentExtractorSpec.scala
+++ src/test/scala/it/dtk/MainContentExtractorSpec.scala
@@ -7,7 +7,6 @@ import akka.actor.Props
 import org.scalatest.{ BeforeAndAfterAll, Matchers, WordSpecLike }
 import it.dtk.util.StepParent
 import scala.concurrent.duration._
-import it.dtk.MainContentExtractor
 
 class MainContentExtractorSpec(_system: ActorSystem) extends TestKit(_system) with ImplicitSender
   with WordSpecLike with Matchers with BeforeAndAfterAll {
