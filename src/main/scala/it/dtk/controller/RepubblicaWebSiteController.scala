97567fb460e6fe5e716bdde4e2d9e4e42d223ab6
fabiofumarola <fabiofumarola@gmail.com>
2014-03-19 09:17:37 +0100

    update to 12 repubblica website controller limit

--- src/main/scala/it/dtk/controller/RepubblicaWebSiteController.scala
+++ src/main/scala/it/dtk/controller/RepubblicaWebSiteController.scala
@@ -11,7 +11,7 @@ import akka.actor.ActorRef
 class RepubblicaWebSiteController(id: String, dbActor: ActorRef, routerHttpGetter: ActorRef)
   extends WebSiteController(id, dbActor, routerHttpGetter) {
 
-  override val maxIndex: Int = 23
+  override val maxIndex: Int = 12
   //override val maxIndex: Int = 5
 
   override val baseUrl: String = "http://bari.repubblica.it/"
