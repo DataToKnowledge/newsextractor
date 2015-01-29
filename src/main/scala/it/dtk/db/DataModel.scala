28617f66feff856e6329a647a44abde28f5792eb
fabiofumarola <fabiofumarola@gmail.com>
2015-01-29 10:18:31 +0100

    setup timezone Europe/Rome

--- src/main/scala/it/dtk/db/DataModel.scala
+++ src/main/scala/it/dtk/db/DataModel.scala
@@ -1,44 +1,42 @@
 package it.dtk.db
 
-import java.util.Date
-import it.dtk.db.DataModel.{ CrawledWebSites, CrawledNews }
-import org.joda.time.DateTime
-import reactivemongo.bson.BSONDocumentReader
-import reactivemongo.bson.BSONDocument
+import org.joda.time.{DateTime, DateTimeZone}
 import reactivemongo.bson.BSONObjectID
-import reactivemongo.bson.BSONDocumentWriter
-import reactivemongo.bson.BSONDateTime
 
 object DataModel {
   type Url = String
   type TagName = String
 
+  val zone = DateTimeZone.forID("Europe/Rome");
+  DateTimeZone.setDefault(zone)
+
   case class CrawledWebSites(
-    id: Option[BSONObjectID] = None,
-    controllerName: Option[String],
-    stopUrls: Option[List[String]],
-    enabled: Option[Boolean] = Option(false))
+                              id: Option[BSONObjectID] = None,
+                              controllerName: Option[String],
+                              stopUrls: Option[List[String]],
+                              enabled: Option[Boolean] = Option(false))
 
   case class CrawledNews(
-      id: Option[String] = None,
-      urlWebSite: String,
-      urlNews: String,
-      title: String,
-      summary: String,
-      newsDate: Option[DateTime],
-      corpus: String,
-      tags: Set[String],
-      metaDescription: String,
-      metaKeyword: String,
-      canonicalUrl: String,
-      topImage: Option[String],
-      nlpAnalyzed: Boolean = false) {
+                          id: Option[String] = None,
+                          urlWebSite: String,
+                          urlNews: String,
+                          title: String,
+                          summary: String,
+                          newsDate: Option[DateTime],
+                          corpus: String,
+                          tags: Set[String],
+                          metaDescription: String,
+                          metaKeyword: String,
+                          canonicalUrl: String,
+                          topImage: Option[String],
+                          nlpAnalyzed: Boolean = false) {
     val extractionDate = DateTime.now
   }
 
   case class DataRecord(
-    title: String,
-    summary: String,
-    newsUrl: String,
-    newsDate: DateTime)
+                         title: String,
+                         summary: String,
+                         newsUrl: String,
+                         newsDate: DateTime)
+
 }
